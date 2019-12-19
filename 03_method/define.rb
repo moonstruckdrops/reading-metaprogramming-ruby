53c7b7d6c42bad0dee2a0fce772f3f654f7e28b3
kurobara <moonstruckdrops@gmail.com>
2019-12-19 14:43:39 +0900

    solve

--- 03_method/define.rb
+++ 03_method/define.rb
@@ -1,6 +1,11 @@
 # Q1.
 # 次の動作をする A1 class を実装する
 # - "//" を返す "//"メソッドが存在すること
+class A1
+  define_method "//" do
+    "//"
+  end
+end
 
 # Q2.
 # 次の動作をする A2 class を実装する
@@ -9,8 +14,39 @@
 # - 2で定義するメソッドは下記とする
 #   - 受け取った引数の回数分、メソッド名を繰り返した文字列を返すこと
 #   - 引数がnilの場合は、dev_teamメソッドを呼ぶこと
+class A2
+  def initialize(names)
+    names.each do|name, i|
+      define_singleton_method("hoge_#{name}") do |arg|
+        arg.nil? ? dev_team : "hoge_#{name}" * arg
+      end
+    end
+  end
+
+  def dev_team
+    "SmartHR Dev Team"
+  end
+end
 
 # Q3.
 # 次の動作をする OriginalAccessor モジュール を実装する
 # - OriginalAccessorモジュールはincludeされたときのみ、my_attr_accessorメソッドを定義すること
 # - my_attr_accessorはgetter/setterに加えて、boolean値を代入した際のみ真偽値判定を行うaccessorと同名の?メソッドができること
+module OriginalAccessor
+  def self.included(mod)
+    mod.define_singleton_method :my_attr_accessor do |attr_sym|
+      define_method attr_sym do
+        @attr
+      end
+
+      define_method "#{attr_sym}=" do |value|
+        if [true, false].include?(value)
+          self.define_singleton_method "#{attr_sym}?" do
+            @attr == true
+          end
+        end
+        @attr = value
+      end
+    end
+  end
+end
